94d186e0a4e06e95a527fce215bcd64c341fd6c0
João Maranhão <joaofelipemaranhao@gmail.com>
2021-04-10 13:45:15 -0300

    feat: create getModels function

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,25 +3,43 @@ import { NextPage } from 'next'
 import { useEffect, useState } from 'react'
 
 const Home: NextPage = () => {
-  const uri = 'https://parallelum.com.br/fipe/api/v1/'
+  const url = 'https://parallelum.com.br/fipe/api/v1/'
   const [vehicle, setVehicle] = useState(false)
   const [brands, setBrands] = useState([])
   const selectVehicle = (vehicle): void => {
     setVehicle(vehicle)
   }
+  const [brandCode, setBrandCode] = useState(undefined)
+  const [models, setModels] = useState([])
+  const [modelCode, setModelCode] = useState(undefined)
 
-  const getBrands = async (): Promise<void> => {
-    const response = await fetch(`${uri + vehicle}/marcas`)
-    const data = await response.json()
-    return setBrands([...data])
+  const http = async (request: RequestInfo): Promise<any> => {
+    const response = await fetch(request)
+    const body = await response.json()
+    return body
+  }
+
+  const getBrands = async (): Promise<any> => {
+    const brands = await http(`${url + vehicle}/marcas`)
+    return setBrands([...brands])
   }
 
   useEffect(() => {
     if (vehicle) {
       getBrands()
-      console.log(brands)
     }
   }, [vehicle])
+
+  const getModels = async (): Promise<any> => {
+    const models = await http(`${url + vehicle}/marcas/${brandCode}/modelos`)
+    return setModels([...models.modelos])
+  }
+
+  useEffect(() => {
+    if (brandCode) {
+      getModels()
+    }
+  }, [brandCode])
 
   return (
     <div>
@@ -39,13 +57,25 @@ const Home: NextPage = () => {
       <div className={vehicle ? 'block' : 'hidden'}>
         <label htmlFor="cars">Selecione a marca:</label>
 
-        <select name="cars" id="cars">
-          <option>Marca</option>
+        <select value={brandCode} onChange={(e) => setBrandCode(e.target.value)}>
+          <option></option>
           {brands.map((brand) => {
-            return <option value={brand.nome} key={brand.codigo}>{brand.nome}</option>
+            return <option value={brand.codigo} key={brand.codigo}>{brand.nome}</option>
           })}
         </select>
       </div>
+
+      <div className={models.length > 0 ? 'block' : 'hidden'}>
+        <label htmlFor="cars">Selecione o modelo:</label>
+
+        <select value={modelCode} onChange={(e) => setModelCode(e.target.value)}>
+          <option></option>
+          {models.map((model) => {
+            return <option value={model.codigo} key={model.codigo}>{model.nome}</option>
+          })}
+        </select>
+      </div>
+
     </div>
   )
 }
