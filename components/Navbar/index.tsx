9d62a96f821d63950617dba362813805b4233146
João Maranhão <joaofelipemaranhao@gmail.com>
2021-04-12 19:47:53 -0300

    style: add style to Navbar

--- components/Navbar/index.tsx
+++ components/Navbar/index.tsx
@@ -3,9 +3,13 @@ import Link from 'next/link'
 
 const Navbar: NextPage = () => {
   return (
-    <nav className="flex">
-      <Link href="/"><a><img src="logo-colors.svg" alt="João Maranhão"/></a></Link>
-      <a href="https://www.joaomaranhao.com.br">Portfolio</a>
+    <nav className="flex p-4 h-20 items-center justify-between bg-transparent">
+      <Link href="/">
+        <a>
+          <img src="logo-colors.svg" alt="João Maranhão" className="h-16"/>
+        </a>
+      </Link>
+      <a href="https://www.joaomaranhao.com.br" className="bg-blue-600 text-gray-100 px-6 py-2 font-semibold hover:bg-blue-500 duration-200">Portfolio</a>
     </nav>
   )
 }
